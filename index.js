34794fbf299590fbc9c7480af015a8239f4156d1
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-06-13 11:17:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function map(sourceArr, callback){
   let newArr = [];
+  for (let i = 0; i < sourceArr.length; i++) {
+    newArr.push(callback(arr[i]));
+  }
+  return newArr;
+}
+
+function reduce(sourceArr, startingValue, ){
   
 }
